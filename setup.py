d62e218bc46b762af2c9a4ab578118d2b42ac878
biswa-mohapatra <jeetmohapatra98@gmail.com>
2023-01-17 23:03:47 +0530

    local setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md","r",encoding="utf-8") as f:
+    long_desc = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Google_scrape"
+AUTHOUR_USER_NAME = "Biswajit"
+SRC_REPO = "python_package"
+AUTHOR_EMAIL = "jeetmohapatra98@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOUR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for craping google search results.",
+    long_description=long_desc,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOUR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker":f"https://github.com/{AUTHOUR_USER_NAME}/{REPO_NAME}/issues"
+    },
+    package_dir={"":"src"},
+    packages=setuptools.find_packages(where="src")
+)
